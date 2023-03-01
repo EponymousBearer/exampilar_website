2f52c0ae4f1904059ef7324066022a7970cb8bd8
EponymousBearer <adnasir607@gmail.com>
2023-03-02 01:21:05 +0500

    Done

---
+++ pages/displayFlex/page.tsx
@@ -0,0 +1,20 @@
+import './page.css';
+
+export default function Page() {
+  return (
+    <div className="flex">
+      <div className="quote">
+      <p>"The greatest glory in living lies not in never falling, but in rising every time we fall"</p>
+      <span>~ Nelson Mandela</span>
+      </div>
+      <div className="quote">
+      <p>"The way to get started is to quit talking and begin doing"</p>
+      <span>~ Walt Disney</span>
+      </div>
+      <div className="quote">
+      <p>"If life were predictable it would cease to be life, and be without flavor"</p>
+      <span>~ Eleanor Roosevelt</span>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 import panaversePic from "../app/panaversePic.jpeg";
 
 export default function Page() {
   return (
-    <>
+    <div>
       <div className="flex justify-center bg-gray-300">
         <div className="mx-4 order-last self-center">
           <Image
@@ -21,11 +22,11 @@ export default function Page() {
             A community of Metaverse and Web 3.0 Development
           </h2>
           <button className="my-4 px-4 py-2 border-2 border-black rounded-lg text-white bg-blue-900 drop-shadow-xl shadow-inner shadow-slate-800">
-            Learn More
+            <Link href={"src\app\page.tsx"}>Quotes</Link>
           </button>
           <div>
             <button className="mx-10 m-2 mb-5 p-2 rounded-xl ring ring-blue-900 ring-offset-2 ring-offset-black bg-white shadow-lg shadow-slate-900">
-              Contact Us
+              <Link href={"/justifybuttons"}>Justify Buttons</Link>
             </button>
             <button className="mx-10 m-2 mb-5 p-2 rounded-xl ring ring-blue-900 ring-offset-2 ring-offset-black bg-white shadow-lg shadow-slate-900">
               About Us
@@ -42,34 +43,108 @@ export default function Page() {
         </button>
       </div>
       <div>
-        <div className="flex">
-          <div className="quote">
-            <p>
-              “The success combination in business is: Do what you do better...
-              and: do more of what you do.”
-            </p>
-            <span>- David J. Schwartz</span>
+        <div className="container flex justify-around flex-wrap">
+          <div className="team-profile">
+            <img
+              src="https://images.pexels.com/photos/7242908/pexels-photo-7242908.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150"
+              className="team-img"
+            />
+            <h3>Alexa Kardi</h3>
+            <p>Founder and CEO</p>
           </div>
-          <div className="quote">
-            <p>“Give out what you most want to come back.”</p>
-            <span>- Robin Sharma</span>
+          <div className="team-profile">
+            <img
+              src="https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150"
+              className="team-img"
+            />
+            <h3>Tavell Monroe</h3>
+            <p>Web Developer</p>
           </div>
-          <div className="quote">
-            <p>
-              “You don't have to be great at something to start, but you have to
-              start to be great at something.”
-            </p>
-            <span>- Zig Ziglar</span>
+          <div className="team-profile">
+            <img
+              src="https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150"
+              className="team-img"
+            />
+            <h3>Adale Smith</h3>
+            <p>Marketing Specialist</p>
           </div>
-          <div className="quote">
-            <p>
-              <h1>Hello</h1>
-              “Nothing to say!”
-            </p>
-            <span>- Adnan</span>
+          <div className="team-profile">
+            <img
+              src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=300"
+              className="team-img"
+            />
+            <h3>Thomas Mason</h3>
+            <p>UX Designer</p>
+          </div>
+          <div className="wrapper">
+            <div className="icon-wrap flex items-stretch">
+              <span className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+                </svg>
+              </span>
+              <span className="icon-text">Stretch</span>
+            </div>
+            <div className="icon-wrap flex items-center">
+              <span className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+                </svg>
+              </span>
+              <span className="icon-text">Center</span>
+            </div>
+            <div className="icon-wrap flex items-start">
+              <span className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+                </svg>
+              </span>
+              <span className="icon-text">Start</span>
+            </div>
+            <div className="icon-wrap flex items-end">
+              <span className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+                </svg>
+              </span>
+              <span className="icon-text">End</span>
+            </div>
+            <div className="icon-wrap flex items-baseline">
+              <span className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+                </svg>
+              </span>
+              <span className="icon-text">Baseline</span>
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
