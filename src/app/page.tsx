38c716d198b214c4add10f6b9b270fff68fc50b7
EponymousBearer <adnasir607@gmail.com>
2023-03-02 01:20:39 +0500

    Pages Created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,6 +41,35 @@ export default function Page() {
           Join Us
         </button>
       </div>
+      <div>
+        <div className="flex">
+          <div className="quote">
+            <p>
+              “The success combination in business is: Do what you do better...
+              and: do more of what you do.”
+            </p>
+            <span>- David J. Schwartz</span>
+          </div>
+          <div className="quote">
+            <p>“Give out what you most want to come back.”</p>
+            <span>- Robin Sharma</span>
+          </div>
+          <div className="quote">
+            <p>
+              “You don't have to be great at something to start, but you have to
+              start to be great at something.”
+            </p>
+            <span>- Zig Ziglar</span>
+          </div>
+          <div className="quote">
+            <p>
+              <h1>Hello</h1>
+              “Nothing to say!”
+            </p>
+            <span>- Adnan</span>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
