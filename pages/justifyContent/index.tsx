1fcadb805bf3ace3bb32d3f804a5efa1ae638111
EponymousBearer <adnasir607@gmail.com>
2023-03-03 23:56:08 +0500

    homepage and till 2a done

--- pages/justifyContent/index.tsx
+++ pages/justifyContent/index.tsx
@@ -1,13 +1,23 @@
-import './index.css';
-import Link from 'next/link';
+import "./index.css";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <div className="wrapper">
-      <button><Link href={"/justifyContent/tabsSpacedOut"}>Tabs Spaced Out</Link></button>
-      <button><Link href={"/justifyContent/cardLinks"}>Card With Previous And Next Link</Link></button>
-      <button><Link href={"/justifyContent/teamProfiles"}>Team Profiles</Link></button>
       Adnan
+      <div className="menu flex justify-between">
+        <button>
+          <Link href={"/justifyContent/tabsSpacedOut"}>Tabs Spaced Out</Link>
+        </button>
+        <button>
+          <Link href={"/justifyContent/cardLinks"}>
+            Card With Previous And Next Link
+          </Link>
+        </button>
+        <button>
+          <Link href={"/justifyContent/teamProfiles"}>Team Profiles</Link>
+        </button>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,54 @@
 import Image from "next/image";
 import Link from "next/link";
-import panaversePic from "../app/panaversePic.jpeg";
+import exampilarlogo from "../app/exampilarlogo.png";
 
 export default function Page2() {
   return (
     <div>
-      <div className="flex justify-center bg-gray-300">
+      <div className="flex justify-center bg-green-100">
         <div className="mx-4 order-last self-center">
           <Image
-            src={panaversePic}
+            src={exampilarlogo}
             height={150}
             width={150}
             alt="music"
           ></Image>
         </div>
         <div className="mx-4 self-center text-center">
-          <h1 className="text-6xl font-bold text-blue-700">
-            Welcome To Panaverse Dao
+          <h1 className="text-6xl font-bold text-green-700">
+            Welcome To Exampilar
           </h1>
-          <h2 className="text-3xl font-semibold text-blue-300">
-            A community of Metaverse and Web 3.0 Development
+          <h2 className="m-3 text-3xl font-semibold text-green-500">
+            Tailwind CSS Example Website
           </h2>
         </div>
       </div>
-      <div className="align-middle justify-center mt-auto bg-gradient-to-r from-blue-500 via-blue-900 to-blue-500">
-        <button>
-          <Link href="/displayFlex">Quotes</Link>
-        </button>
-        <button>
-          <Link href={"/justifyContent"}>Justify Content</Link>
-        </button>
-        <button>
-          <Link href={"/flexShrink"}>Flex Shrink</Link>
-        </button>
-        <button>
-          <Link href={"/flexGrow"}>Flex Grow</Link>
-        </button>
-        <button>
-          <Link href={"/flexWrap"}>Flex Wrap</Link>
-        </button>
-        <button>
-          <Link href={"/flexDirection"}>Flex Direction</Link>
-        </button>
-        <button>
-          <Link href={"/alignItems"}>Align Items</Link>
-        </button>
+      <div className="bg-gradient-to-r from-green-100 via-green-500 to-green-100">
+        <div className="flex align-middle justify-between">
+          <button>
+            <Link href="/displayFlex">Quotes</Link>
+          </button>
+          <button>
+            <Link href={"/justifyContent"}>Justify Content</Link>
+          </button>
+          <button>
+            <Link href={"/flexShrink"}>Flex Shrink</Link>
+          </button>
+          <button>
+            <Link href={"/flexGrow"}>Flex Grow</Link>
+          </button>
+        </div>
+        <div className="flex mx-3 align-middle justify-around">
+          <button>
+            <Link href={"/flexWrap"}>Flex Wrap</Link>
+          </button>
+          <button>
+            <Link href={"/flexDirection"}>Flex Direction</Link>
+          </button>
+          <button>
+            <Link href={"/alignItems"}>Align Items</Link>
+          </button>
+        </div>
       </div>
     </div>
   );
